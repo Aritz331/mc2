e5d9a07f5a4d3451dab00bd35ef1b49e8fd6ad18
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-05-08 21:13:54 +0200

    Update mcp.bat

--- mcp.bat
+++ mcp.bat
@@ -1,7 +1,10 @@
-@echo ^> @echo foo bar- i mean off :sweat_smile:
-@echo.off
-@echo - wtf
-@echo off
+rem @echo off
+echo %1 | findstr /b /i "dl" >nul && (
+  goto %1
+)
+echo ^> @echo foo bar- i mean off :sweat_smile:
+echo.off
+echo - wtf
 timeout 3 /nobreak >nul 2>&1
 cls
 
@@ -17,19 +20,34 @@ for /f "tokens=*" %%i in ('curl -kLs aritz331.tk/mc2/fol%1.txt') do (
 exit /b
 
 :dl
-for /l %%i in (1, 1, 3222) do (
-  curl -kLO "aritz331.tk/mc2/m-%%i.b" --progress-bar
-  cls
-)
-for /l %%i in (3223, 1, 3639) do (
-  curl -kLO "aritz331.tk/mc2/j-%%i.b" --progress-bar
-  cls
-)
+start /min "" cmd /c "%~dpnx0" dlm
+start /wait /min "" cmd /c "%~dpnx0" dlj
 
+goto cont1
+
+:dlm
+echo dlm: downloading...
+if not exist m-3222.b (start /min "" cmd /c curl -kLO "aritz331.tk/mc2/m-[1-805].b" --progress-bar)
+if not exist m-3222.b (start /min "" cmd /c curl -kLO "aritz331.tk/mc2/m-[805-1610].b" --progress-bar)
+if not exist m-3222.b (start /min "" cmd /c curl -kLO "aritz331.tk/mc2/m-[1610-2415].b" --progress-bar)
+if not exist m-3222.b (start /min "" cmd /c curl -kLO "aritz331.tk/mc2/m-[2415-3222].b" --progress-bar)
+exit /b
+
+:dlj
+echo dlj: downloading...
+if not exist j-3639.b (curl -kLO "aritz331.tk/mc2/j-[3222-3326].b" --progress-bar)
+if not exist j-3639.b (curl -kLO "aritz331.tk/mc2/j-[3326-3430].b" --progress-bar)
+if not exist j-3639.b (curl -kLO "aritz331.tk/mc2/j-[3430-3534].b" --progress-bar)
+if not exist j-3639.b (curl -kLO "aritz331.tk/mc2/j-[3534-3639].b" --progress-bar)
+exit /b
+
+:cont1
 setlocal enableextensions enabledelayedexpansion
 
 call :move 1 m
 call :move 2 j
+
+goto cont2
 
 :move
 curl -kLO "aritz331.tk/mc2/folf%1.txt"
@@ -44,17 +62,16 @@ exit /b
 
 :cont2
 endlocal
-pause 
 
 :mc
 set "jvp=.\j\bin\javaw.exe" // java path
 set "mcp=.\mc" // mc path
 set "mcv=fabric-loader-0.14.0-1.18.2" // mc ver.
 echo usrnaem:
-set /p "usr=> "
+set /p "usrn=> "
 
 :launch
-start /wait "" "%jvp%" -Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -XX:+IgnoreUnrecognizedVMOptions -Dorg.lwjgl.opengl.Display.allowSoftwareOpenGL=true -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump -Djava.library.path=%mcp%\versions\%mcv%\%mcv%-natives-1177346109300 -Dminecraft.launcher.brand=minecraft-launcher -Dminecraft.launcher.version=2.1.5964 -cp %mcp%\libraries\com\mojang\laulib\1.0\laulib-1.0.jar;%mcp%\libraries\net\fabricmc\tiny-mappings-parser\0.3.0+build.17\tiny-mappings-parser-0.3.0+build.17.jar;%mcp%\libraries\net\fabricmc\sponge-mixin\0.11.3+mixin.0.8.5\sponge-mixin-0.11.3+mixin.0.8.5.jar;%mcp%\libraries\net\fabricmc\tiny-remapper\0.8.2\tiny-remapper-0.8.2.jar;%mcp%\libraries\net\fabricmc\access-widener\2.1.0\access-widener-2.1.0.jar;%mcp%\libraries\org\ow2\asm\asm\9.3\asm-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-analysis\9.3\asm-analysis-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-commons\9.3\asm-commons-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-tree\9.3\asm-tree-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-util\9.3\asm-util-9.3.jar;%mcp%\libraries\net\fabricmc\intermediary\1.18.2\intermediary-1.18.2.jar;%mcp%\libraries\net\fabricmc\fabric-loader\0.14.0\fabric-loader-0.14.0.jar;%mcp%\libraries\com\mojang\logging\1.0.0\logging-1.0.0.jar;%mcp%\libraries\com\mojang\blocklist\1.0.10\blocklist-1.0.10.jar;%mcp%\libraries\com\mojang\patchy\2.2.10\patchy-2.2.10.jar;%mcp%\libraries\com\github\oshi\oshi-core\5.8.5\oshi-core-5.8.5.jar;%mcp%\libraries\net\java\dev\jna\jna\5.10.0\jna-5.10.0.jar;%mcp%\libraries\net\java\dev\jna\jna-platform\5.10.0\jna-platform-5.10.0.jar;%mcp%\libraries\org\slf4j\slf4j-api\1.8.0-beta4\slf4j-api-1.8.0-beta4.jar;%mcp%\libraries\org\apache\logging\log4j\log4j-slf4j18-impl\2.17.0\log4j-slf4j18-impl-2.17.0.jar;%mcp%\libraries\com\ibm\icu\icu4j\70.1\icu4j-70.1.jar;%mcp%\libraries\com\mojang\javabridge\1.2.24\javabridge-1.2.24.jar;%mcp%\libraries\net\sf\jopt-simple\jopt-simple\5.0.4\jopt-simple-5.0.4.jar;%mcp%\libraries\io\netty\netty-all\4.1.68.Final\netty-all-4.1.68.Final.jar;%mcp%\libraries\com\google\guava\failureaccess\1.0.1\failureaccess-1.0.1.jar;%mcp%\libraries\com\google\guava\guava\31.0.1-jre\guava-31.0.1-jre.jar;%mcp%\libraries\org\apache\commons\commons-lang3\3.12.0\commons-lang3-3.12.0.jar;%mcp%\libraries\commons-io\commons-io\2.11.0\commons-io-2.11.0.jar;%mcp%\libraries\commons-codec\commons-codec\1.15\commons-codec-1.15.jar;%mcp%\libraries\com\mojang\brigadier\1.0.18\brigadier-1.0.18.jar;%mcp%\libraries\com\mojang\datafixerupper\4.1.27\datafixerupper-4.1.27.jar;%mcp%\libraries\com\google\code\gson\gson\2.8.9\gson-2.8.9.jar;%mcp%\libraries\com\mojang\authlib\3.3.39\authlib-3.3.39.jar;%mcp%\libraries\org\apache\commons\commons-compress\1.21\commons-compress-1.21.jar;%mcp%\libraries\org\apache\httpcomponents\httpclient\4.5.13\httpclient-4.5.13.jar;%mcp%\libraries\commons-logging\commons-logging\1.2\commons-logging-1.2.jar;%mcp%\libraries\org\apache\httpcomponents\httpcore\4.4.14\httpcore-4.4.14.jar;%mcp%\libraries\it\unimi\dsi\fastutil\8.5.6\fastutil-8.5.6.jar;%mcp%\libraries\org\apache\logging\log4j\log4j-api\2.17.0\log4j-api-2.17.0.jar;%mcp%\libraries\org\apache\logging\log4j\log4j-core\2.17.0\log4j-core-2.17.0.jar;%mcp%\libraries\org\lwjgl\lwjgl\3.2.2\lwjgl-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-jemalloc\3.2.2\lwjgl-jemalloc-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-openal\3.2.2\lwjgl-openal-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-opengl\3.2.2\lwjgl-opengl-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-glfw\3.2.2\lwjgl-glfw-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-stb\3.2.2\lwjgl-stb-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-tinyfd\3.2.2\lwjgl-tinyfd-3.2.2.jar;%mcp%\libraries\com\mojang\text2speech\1.12.4\text2speech-1.12.4.jar;%mcp%\versions\%mcv%\%mcv%.jar "-DFabricMcEmu= net.minecraft.client.main.Main " net.fabricmc.loader.impl.launch.knot.KnotClient --username %usr% --version %mcv% --gameDir %mcp% --assetsDir %mcp%\assets --assetIndex 1.18 --uuid 0004c61756e6468657246656e6978000 --accessToken accessToken --clientId ${clientid} --xuid ${auth_xuid} --userType legacy --versionType release
+start /wait "" "%jvp%" -Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -XX:+IgnoreUnrecognizedVMOptions -Dorg.lwjgl.opengl.Display.allowSoftwareOpenGL=true -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump -Djava.library.path=%mcp%\versions\%mcv%\%mcv%-natives-1177346109300 -Dminecraft.launcher.brand=minecraft-launcher -Dminecraft.launcher.version=2.1.5964 -cp %mcp%\libraries\com\mojang\laulib\1.0\laulib-1.0.jar;%mcp%\libraries\net\fabricmc\tiny-mappings-parser\0.3.0+build.17\tiny-mappings-parser-0.3.0+build.17.jar;%mcp%\libraries\net\fabricmc\sponge-mixin\0.11.3+mixin.0.8.5\sponge-mixin-0.11.3+mixin.0.8.5.jar;%mcp%\libraries\net\fabricmc\tiny-remapper\0.8.2\tiny-remapper-0.8.2.jar;%mcp%\libraries\net\fabricmc\access-widener\2.1.0\access-widener-2.1.0.jar;%mcp%\libraries\org\ow2\asm\asm\9.3\asm-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-analysis\9.3\asm-analysis-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-commons\9.3\asm-commons-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-tree\9.3\asm-tree-9.3.jar;%mcp%\libraries\org\ow2\asm\asm-util\9.3\asm-util-9.3.jar;%mcp%\libraries\net\fabricmc\intermediary\1.18.2\intermediary-1.18.2.jar;%mcp%\libraries\net\fabricmc\fabric-loader\0.14.0\fabric-loader-0.14.0.jar;%mcp%\libraries\com\mojang\logging\1.0.0\logging-1.0.0.jar;%mcp%\libraries\com\mojang\blocklist\1.0.10\blocklist-1.0.10.jar;%mcp%\libraries\com\mojang\patchy\2.2.10\patchy-2.2.10.jar;%mcp%\libraries\com\github\oshi\oshi-core\5.8.5\oshi-core-5.8.5.jar;%mcp%\libraries\net\java\dev\jna\jna\5.10.0\jna-5.10.0.jar;%mcp%\libraries\net\java\dev\jna\jna-platform\5.10.0\jna-platform-5.10.0.jar;%mcp%\libraries\org\slf4j\slf4j-api\1.8.0-beta4\slf4j-api-1.8.0-beta4.jar;%mcp%\libraries\org\apache\logging\log4j\log4j-slf4j18-impl\2.17.0\log4j-slf4j18-impl-2.17.0.jar;%mcp%\libraries\com\ibm\icu\icu4j\70.1\icu4j-70.1.jar;%mcp%\libraries\com\mojang\javabridge\1.2.24\javabridge-1.2.24.jar;%mcp%\libraries\net\sf\jopt-simple\jopt-simple\5.0.4\jopt-simple-5.0.4.jar;%mcp%\libraries\io\netty\netty-all\4.1.68.Final\netty-all-4.1.68.Final.jar;%mcp%\libraries\com\google\guava\failureaccess\1.0.1\failureaccess-1.0.1.jar;%mcp%\libraries\com\google\guava\guava\31.0.1-jre\guava-31.0.1-jre.jar;%mcp%\libraries\org\apache\commons\commons-lang3\3.12.0\commons-lang3-3.12.0.jar;%mcp%\libraries\commons-io\commons-io\2.11.0\commons-io-2.11.0.jar;%mcp%\libraries\commons-codec\commons-codec\1.15\commons-codec-1.15.jar;%mcp%\libraries\com\mojang\brigadier\1.0.18\brigadier-1.0.18.jar;%mcp%\libraries\com\mojang\datafixerupper\4.1.27\datafixerupper-4.1.27.jar;%mcp%\libraries\com\google\code\gson\gson\2.8.9\gson-2.8.9.jar;%mcp%\libraries\com\mojang\authlib\3.3.39\authlib-3.3.39.jar;%mcp%\libraries\org\apache\commons\commons-compress\1.21\commons-compress-1.21.jar;%mcp%\libraries\org\apache\httpcomponents\httpclient\4.5.13\httpclient-4.5.13.jar;%mcp%\libraries\commons-logging\commons-logging\1.2\commons-logging-1.2.jar;%mcp%\libraries\org\apache\httpcomponents\httpcore\4.4.14\httpcore-4.4.14.jar;%mcp%\libraries\it\unimi\dsi\fastutil\8.5.6\fastutil-8.5.6.jar;%mcp%\libraries\org\apache\logging\log4j\log4j-api\2.17.0\log4j-api-2.17.0.jar;%mcp%\libraries\org\apache\logging\log4j\log4j-core\2.17.0\log4j-core-2.17.0.jar;%mcp%\libraries\org\lwjgl\lwjgl\3.2.2\lwjgl-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-jemalloc\3.2.2\lwjgl-jemalloc-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-openal\3.2.2\lwjgl-openal-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-opengl\3.2.2\lwjgl-opengl-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-glfw\3.2.2\lwjgl-glfw-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-stb\3.2.2\lwjgl-stb-3.2.2.jar;%mcp%\libraries\org\lwjgl\lwjgl-tinyfd\3.2.2\lwjgl-tinyfd-3.2.2.jar;%mcp%\libraries\com\mojang\text2speech\1.12.4\text2speech-1.12.4.jar;%mcp%\versions\%mcv%\%mcv%.jar "-DFabricMcEmu= net.minecraft.client.main.Main " net.fabricmc.loader.impl.launch.knot.KnotClient --username %usrn% --version %mcv% --gameDir %mcp% --assetsDir %mcp%\assets --assetIndex 1.18 --uuid 0004c61756e6468657246656e6978000 --accessToken accessToken --clientId ${clientid} --xuid ${auth_xuid} --userType legacy --versionType release
 
 del m-*.b
 del j-*.b
